5bd5f81ca722307c9309400c588b186f088c0007
João Oliveira <deoliveira.jvm@gmail.com>
2025-12-10 22:57:41 -0300

    💄 Change app font to Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const appFont = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Travelapp by Excursia Viagens - Roteiros Inteligentes",
@@ -57,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={appFont.className}>
       <head>
         <meta
           name="viewport"
